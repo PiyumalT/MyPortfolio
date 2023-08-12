13187237d73719aee6cf4f1b1de432ac4be7c7be
PiyumalT <piyumal-se19035@stu.kln.ac.lk>
2023-08-12 23:36:48 +0530

    accordion added for data list

--- script.js
+++ script.js
@@ -19,3 +19,37 @@ function scrollToTop() {
         behavior: 'smooth'
     });
 }
+
+function show_hide_list(event) {
+    const parentSection = event.currentTarget.parentElement;
+    var data_set = parentSection.querySelector(".data_set");
+    var expand_icon = parentSection.querySelector(".expand_icon");
+    
+    if (data_set.style.maxHeight === "0px") {
+        // data_set.style.maxHeight = "1000px"; // Set max-height to your desired value
+        expand_icon.innerHTML = '<i class="fas fa-caret-up"></i>';
+
+        for (var i = 0; i <= 30; i++) {
+            setTimeout(function(index) {
+                return function() {
+                    var newHeight = (index * 12);
+                    data_set.style.maxHeight = newHeight + 'px';
+                };
+            }(i), i * 10);
+        }
+    } else {
+        // data_set.style.maxHeight = "0"; // Set max-height to 0
+        expand_icon.innerHTML = '<i class="fas fa-caret-down"></i>';
+        for (var i = 0; i <= 50; i++) {
+            setTimeout(function(index) {
+                return function() {
+                    var newHeight =300- (index * 15);
+                    data_set.style.maxHeight = newHeight + 'px';
+                };
+            }(i), i * 10);
+        }
+    }    
+}
+
+
+  
