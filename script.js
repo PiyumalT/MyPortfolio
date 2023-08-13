85b03120e2b2aebe41eefbcaaf5170915e9b4783
PiyumalT <piyumal-se19035@stu.kln.ac.lk>
2023-08-13 15:03:06 +0530

    send message function added

--- script.js
+++ script.js
@@ -112,4 +112,27 @@ window.addEventListener("scroll", () => {
     prevScrollPos = currentScrollPos;
 });
 
+
+const openModalButton = document.getElementById("openModal");
+const modalOverlay = document.querySelector(".modal-overlay");
+const closeButton = document.querySelector(".close");
+
+openModalButton.addEventListener("click", () => {
+    modalOverlay.style.display = "block";
+});
+
+closeButton.addEventListener("click", () => {
+    modalOverlay.style.display = "none";
+});
+
+modalOverlay.addEventListener("click", (event) => {
+    if (event.target === modalOverlay) {
+        modalOverlay.style.display = "none";
+    }
+});
+
+function openModal() {
+    modalOverlay.style.display = "block";
+}
+
   
